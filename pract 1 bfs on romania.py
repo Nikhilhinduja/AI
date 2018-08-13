dffa725ce93af247fa382c48471a83fd11709c00
Nikhilhinduja <Nikhilhinduja86@gmail.com>
2018-08-13 00:54:21 -0400

    Add files via upload

---
+++ pract 1 bfs on romania.py
@@ -0,0 +1,163 @@
+from collections import deque
+
+infinity = float('inf')
+
+class Node:
+    def __init__(self, state, parent=None, action=None, path_cost=0):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.path_cost = path_cost
+        self.depth = 0
+        if parent:
+            self.depth = parent.depth + 1
+
+    def __repr__(self):
+        return "<Node {}>".format(self.state)
+
+    def __lt__(self, node):
+        return self.state < node.state
+
+    def expand(self, problem):
+        return [self.child_node(problem, action)
+                for action in problem.actions(self.state)]
+
+    def child_node(self, problem, action):
+        next_state = problem.result(self.state, action)
+        next_node = Node(next_state, self, action,
+                    problem.path_cost(self.path_cost, self.state,
+                                      action, next_state))
+        return next_node
+    
+    def solution(self):
+        return [node.action for node in self.path()[1:]]
+
+    def path(self):
+        node, path_back = self, []
+        while node:
+            path_back.append(node)
+            node = node.parent
+        return list(reversed(path_back))
+    def __eq__(self, other):
+        return isinstance(other, Node) and self.state == other.state
+
+    def __hash__(self):
+        return hash(self.state)
+
+class Graph:
+    def __init__(self, graph_dict=None, directed=True):
+        self.graph_dict = graph_dict or {}
+        self.directed = directed
+        if not directed:
+            self.make_undirected()
+
+    def make_undirected(self):
+        for a in list(self.graph_dict.keys()):
+            for (b, dist) in self.graph_dict[a].items():
+                self.connect1(b, a, dist)
+
+    def connect(self, A, B, distance=1):
+        self.connect1(A, B, distance)
+        if not self.directed:
+            self.connect1(B, A, distance)
+
+    def connect1(self, A, B, distance):
+        self.graph_dict.setdefault(A, {})[B] = distance
+
+    def get(self, a, b=None):
+        links = self.graph_dict.setdefault(a, {})
+        if b is None:
+            return links
+        else:
+            return links.get(b)
+
+    def nodes(self):        
+        s1 = set([k for k in self.graph_dict.keys()])
+        s2 = set([k2 for v in self.graph_dict.values() for k2, v2 in v.items()])
+        nodes = s1.union(s2)
+        return list(nodes)
+
+
+def UndirectedGraph(graph_dict=None):
+    return Graph(graph_dict = graph_dict, directed=False)
+
+
+class Problem(object):
+    def __init__(self, initial, goal=None):
+       self.initial = initial
+       self.goal = goal
+
+    def actions(self, state):
+         raise NotImplementedError
+
+    def result(self, state, action):
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    def path_cost(self, c, state1, action, state2):
+        return c + 1
+
+    def value(self, state):
+        raise NotImplementedError
+
+class GraphProblem(Problem):
+    def __init__(self, initial, goal, graph):
+        Problem.__init__(self, initial, goal)
+        self.graph = graph
+
+    def actions(self, A):
+        return list(self.graph.get(A).keys())
+
+    def result(self, state, action):
+        return action
+
+    def path_cost(self, cost_so_far, A, action, B):
+        return cost_so_far + (self.graph.get(A, B) or infinity)
+
+    def find_min_edge(self):
+        m = infinity
+        for d in self.graph.graph_dict.values():
+            local_min = min(d.values())
+            m = min(m, local_min)
+
+        return m
+
+    def h(self, node):
+        locs = getattr(self.graph, 'locations', None)
+        if locs:
+            if type(node) is str:
+                return int(distance(locs[node], locs[self.goal]))
+
+            return int(distance(locs[node.state], locs[self.goal]))
+        else:
+            return infinity
+
+def breadth_first_tree_search(problem):
+    frontier = deque([Node(problem.initial)])
+    print("starting from : ", frontier)
+    while frontier:
+        node = frontier.popleft()
+        print("going to :", node)
+        if problem.goal_test(node.state):
+            return node
+        frontier.extend(node.expand(problem))
+    return None
+
+
+mumbai_map = UndirectedGraph(dict(
+    
+    kurla=dict(sion=5,santacruz=2),
+    sion=dict(kurla=5,bandra=3,dadar=4),
+    dadar=dict(sion=4,bandra=4),
+    bandra=dict(santacruz=3,sion=3,dadar=4),
+    santacruz=dict(bandra=3,kurla=2)))
+
+
+mumbai_problem = GraphProblem('kurla','dadar', mumbai_map)
+print(breadth_first_tree_search(mumbai_problem))
+
